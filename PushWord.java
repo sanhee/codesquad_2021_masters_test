1e3968ddc6dcf96de948d5bc55c9f074d7e7f82a
noel <68936@naver.com>
2020-12-08 02:31:54 +0900

    Implement cleanup code

--- PushWord.java
+++ PushWord.java
@@ -4,63 +4,61 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/***
- * 입력: 사용자로부터 단어 하나, 정수 숫자 하나( -100 <= N < 100) , L 또는 R을 입력받는다. L 또는 R은 대소문자 모두 입력 가능하다.
- * 주어진 단어를 L이면 주어진 숫자 갯수만큼 왼쪽으로, R이면 오른쪽으로 밀어낸다.
- * 밀려나간 단어는 반대쪽으로 채워진다.
- */
 public class PushWord {
 
     public static void main(String[] args) {
 
-        StringBuffer ShiftStr = new StringBuffer();
-        String[] InputStr;  //m 입력 문자열
+        String[] KeyboardInput;  //m 입력 문자열 배열
+        StringBuffer ShiftResult = new StringBuffer();
         int ShiftNum = 0;   //m 입력된 문자 이동 횟수
         String ShiftDirection = "";   //m 입력된 문자 이동 방향
         Boolean[] CheckDirection = {false,false}; //m 음수의 경우, 문자 이동 방향을 반대로 하기 위한 상태 불린
-        final int LEFT = 0; //m 매직넘버를 없애기 위한, 상수로 CheckDirection 불린 배열의 방향을 표시함.
-        final int RIGHT = 1;
+
+        //m 매직넘버를 없애기 위한 상수들
+        final int LEFT = 0;  //m CheckDirection 불린 배열 구분자 역할
+        final int RIGHT = 1;  //m CheckDirection 불린 배열 구분자 역할
+        final int CONTENTS = 0; //m KeyboardInput 스트링 배열 구분자 역할
+        final int SHIFT_TIME = 1; //m KeyboardInput 스트링 배열 구분자 역할
+        final int SHIFT_DIRECTION = 2; //m KeyboardInput 스트링 배열 구분자 역할
 
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            InputStr = br.readLine().split(" ");
-            br.close(); //m Close BufferReader
 
-            ShiftStr.insert(0,InputStr[0]);
-            ShiftNum = Integer.parseInt(InputStr[1]);
-            ShiftDirection = InputStr[2];
+            KeyboardInput = br.readLine().split(" "); //m 공백 기준 입력값 짤라서 배열에 삽입
 
-            CheckDirection[LEFT] = ShiftDirection.equalsIgnoreCase("L");  //m 대소문자 상관없이 비교하는 equalsIgnoreCase() 메소드 사용
-            CheckDirection[RIGHT] = ShiftDirection.equalsIgnoreCase("R");
+            ShiftResult.insert(0,KeyboardInput[CONTENTS]); //m 쉬프팅 할 문자열
+            ShiftNum = Integer.parseInt(KeyboardInput[SHIFT_TIME]); //m 쉬프팅 횟수
+            ShiftDirection = KeyboardInput[SHIFT_DIRECTION]; //m 쉬프팅 방향
 
-            //m 쉬프트 횟수가 음수 일 경우, 방향 정보를 반대로 바꾸기 위한 조건문
-            if(( (CheckDirection[LEFT] || CheckDirection[RIGHT]) && ShiftNum < 0)) {
-                CheckDirection[RIGHT] = !CheckDirection[RIGHT]; //m 불린값 반전
-                CheckDirection[LEFT] = !CheckDirection[LEFT]; //m 불린값 반전
-                ShiftNum = Math.abs(ShiftNum);
-            }
-
-            //m 방향에 따른 조건문 형식
-            if(CheckDirection[LEFT]) { //m 왼쪽 쉬프트인 경우
-                for(int i=0; i<ShiftNum; i++){
-                   ShiftStr.append(ShiftStr.charAt(0));
-                   ShiftStr.deleteCharAt(0);
-                }
-            }
-            else if(CheckDirection[RIGHT]) { //m 오른쪽 쉬프트인 경우
-                for(int i=0; i<ShiftNum; i++){
-                   ShiftStr.insert(0, ShiftStr.charAt(ShiftStr.length() - 1));
-                   ShiftStr.deleteCharAt(ShiftStr.length()-1);
-                }
-            }
-            System.out.println(ShiftStr);
-
-
+            br.close(); //m Close BufferedReader
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        //m 대소문자 상관없이 비교하는 equalsIgnoreCase() 메소드 사용
+        CheckDirection[LEFT] = ShiftDirection.equalsIgnoreCase("L");
+        CheckDirection[RIGHT] = ShiftDirection.equalsIgnoreCase("R");
 
+        //m 쉬프트 횟수가 음수 일 경우, 방향 정보를 반대로 바꾸기 위한 조건문
+        if(( (CheckDirection[LEFT] || CheckDirection[RIGHT]) && ShiftNum < 0)) {
+            CheckDirection[RIGHT] = !CheckDirection[RIGHT]; //m 불린값 반전
+            CheckDirection[LEFT] = !CheckDirection[LEFT]; //m 불린값 반전
+            ShiftNum = Math.abs(ShiftNum); //m 음수 절대값으로 변환
+        }
+        //m 왼쪽 쉬프트인 경우
+        if(CheckDirection[LEFT]) {
+            for(int i=0; i<ShiftNum; i++){
+                ShiftResult.append(ShiftResult.charAt(0));
+                ShiftResult.deleteCharAt(0);
+            }
+        }
+        //m 오른쪽 쉬프트인 경우
+        else if(CheckDirection[RIGHT]) {
+            for(int i=0; i<ShiftNum; i++){
+                ShiftResult.insert(0, ShiftResult.charAt(ShiftResult.length() - 1));
+                ShiftResult.deleteCharAt(ShiftResult.length()-1);
+            }
+        }
+        System.out.println(ShiftResult); //m 쉬프팅 결과 출력
     }
-
 }
