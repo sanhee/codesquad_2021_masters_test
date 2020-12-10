72a056f849009d9e0ded7b38becdc2d87f73c0f7
noel <68936@naver.com>
2020-12-10 18:05:47 +0900

    Added Infinite Input and Exit Command in FlatCube.java

--- FlatCube.java
+++ FlatCube.java
@@ -11,34 +11,39 @@ public class FlatCube {
 
         PrintCube PrintCube = new PrintCube();
 
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("CUBE> "); //m CLI 입력 표시
-            String[] KeyInputCommand = br.readLine().split(""); //m 입력값을 한글자씩 잘라서 배열에 삽입함.
+        while(true) {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                System.out.print("CUBE> "); //m CLI 입력 표시
+                String[] KeyInputCommand = br.readLine().split(""); //m 입력값을 한글자씩 잘라서 배열에 삽입함.
 
-
-            for(int i=0 ; i<KeyInputCommand.length;i++){
-
-                if(i != KeyInputCommand.length-1 && KeyInputCommand[i+1].equals("`") ) {  //m 다음 배열 순번의 데이터값이 `일 경우, 이어 붙여서 출력함.
-                    System.out.println(KeyInputCommand[i]+KeyInputCommand[i+1]);
-                    PrintCube.SetShiftType(KeyInputCommand[i]+KeyInputCommand[i+1]);
-                    PrintCube.SetCubeArray();
-                    i++; //m  `이후부터 출력하기 위해 사용한 증감식
-                }
-                else {
-                    System.out.println(KeyInputCommand[i]);
-                    PrintCube.SetShiftType(KeyInputCommand[i]);
-                    PrintCube.SetCubeArray();
+                if ( KeyInputCommand[0].equals("Q") ){
+                    System.out.println("Bye~");
+                    br.close(); //m Close BufferedReader
+                    break;
                 }
 
-                PrintCube.PrintCube();
+                for (int i = 0; i < KeyInputCommand.length; i++) {
 
+                    if (i != KeyInputCommand.length - 1 && KeyInputCommand[i + 1].equals("`")) {  //m 다음 배열 순번의 데이터값이 `일 경우, 이어 붙여서 출력함.
+                        System.out.println(KeyInputCommand[i] + KeyInputCommand[i + 1]);
+                        PrintCube.SetShiftType(KeyInputCommand[i] + KeyInputCommand[i + 1]);
+                        PrintCube.SetCubeArray();
+                        i++; //m  `이후부터 출력하기 위해 사용한 증감식
+                    } else {
+                        System.out.println(KeyInputCommand[i]);
+                        PrintCube.SetShiftType(KeyInputCommand[i]);
+                        PrintCube.SetCubeArray();
+                    }
+
+                    PrintCube.PrintCube();
+                    System.out.println();
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            br.close(); //m Close BufferedReader
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
 
 
     }
