0e4f57f6d568a81278e24a46504e4fd3018282ed
noel <68936@naver.com>
2020-12-11 16:31:19 +0900

    Added KeyboardInput and CheckExit method from CubeMain.java

---
+++ CubeMain.java
@@ -0,0 +1,39 @@
+package com.sanhee.step3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CubeMain {
+
+    public void CheckExit(String exit){
+        if ( exit.equals("Q") ){
+            System.out.println("Bye~");
+            System.exit(0);
+        }
+    }
+
+    public void KeyboardInput(){
+
+        while(true) {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                System.out.print("CUBE> "); //m CLI 입력 표시
+                String[] KeyInputCommand = br.readLine().split(""); //m 입력값을 한글자씩 잘라서 배열에 삽입함.
+
+                CheckExit(KeyInputCommand[0]); //m 종료(Q) 검사
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        CubeMain cubeMain = new CubeMain();
+        cubeMain.KeyboardInput();
+
+
+    }
+
+}
